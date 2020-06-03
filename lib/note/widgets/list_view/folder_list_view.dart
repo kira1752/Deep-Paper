e881ef2d34707ff68f79c553475b2aac6e632b56
kira1752 <hendradlucky4@gmail.com>
2020-06-03 17:11:39 +0800

    refactor illustration code

--- lib/note/widgets/list_view/folder_list_view.dart
+++ lib/note/widgets/list_view/folder_list_view.dart
@@ -1,13 +1,11 @@
 import 'package:deep_paper/note/data/deep.dart';
+import 'package:deep_paper/note/widgets/empty_note_illustration.dart';
 import 'package:deep_paper/note/widgets/note_card.dart';
 import 'package:deep_paper/utility/deep_keep_alive.dart';
-import 'package:deep_paper/utility/illustration.dart';
-import 'package:deep_paper/utility/size_helper.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:deep_paper/utility/extension.dart';
-import 'package:responsive_widgets/responsive_widgets.dart';
 
 class FolderListView extends StatelessWidget {
   final FolderNoteData folder;
@@ -26,46 +24,7 @@ class FolderListView extends StatelessWidget {
           child: data.isNull
               ? const SizedBox()
               : data.isEmpty
-                  ? Center(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Image.asset(
-                            Illustration.getNote,
-                            width: SizeHelper.setWidth(size: 220.0),
-                            height: SizeHelper.setHeight(size: 220.0),
-                          ),
-                          Padding(
-                            padding: EdgeInsetsResponsive.only(top: 24.0),
-                            child: RichText(
-                              text: TextSpan(children: <TextSpan>[
-                                TextSpan(
-                                  text: "Never forget anything\n",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline6
-                                      .copyWith(
-                                          color: Colors.white70,
-                                          fontSize: SizeHelper.getHeadline5,
-                                          fontWeight: FontWeight.bold),
-                                ),
-                                TextSpan(
-                                  text: "Write all your important things",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .copyWith(
-                                          color: Colors.white70,
-                                          fontSize: SizeHelper.getBodyText1,
-                                          fontWeight: FontWeight.w400),
-                                )
-                              ]),
-                              textAlign: TextAlign.center,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
+                  ? EmptyNoteIllustration()
                   : ListView.builder(
                       physics: const ClampingScrollPhysics(),
                       itemCount: data.length,
--- lib/note/widgets/list_view/trash_list_view.dart
+++ lib/note/widgets/list_view/trash_list_view.dart
@@ -1,14 +1,12 @@
 import 'package:deep_paper/note/data/deep.dart';
 import 'package:deep_paper/note/widgets/bottom_modal.dart';
+import 'package:deep_paper/note/widgets/empty_trash_illustration.dart';
 import 'package:deep_paper/note/widgets/note_card.dart';
 import 'package:deep_paper/utility/deep_keep_alive.dart';
-import 'package:deep_paper/utility/illustration.dart';
-import 'package:deep_paper/utility/size_helper.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:deep_paper/utility/extension.dart';
-import 'package:responsive_widgets/responsive_widgets.dart';
 
 class TrashListView extends StatelessWidget {
   @override
@@ -23,32 +21,7 @@ class TrashListView extends StatelessWidget {
           child: data.isNull
               ? const SizedBox()
               : data.isEmpty
-                  ? Center(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Image.asset(
-                            Illustration.getTrash,
-                            width: SizeHelper.setWidth(size: 220.0),
-                            height: SizeHelper.setHeight(size: 200.0),
-                          ),
-                          Padding(
-                            padding: EdgeInsetsResponsive.only(top: 24.0),
-                            child: Text(
-                              "Your trash bin is clean",
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6
-                                  .copyWith(
-                                      color: Colors.white70,
-                                      fontSize: SizeHelper.getHeadline5,
-                                      fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
+                  ? EmptyTrashIllustration()
                   : ListView.builder(
                       physics: const ClampingScrollPhysics(),
                       itemCount: data.length,
